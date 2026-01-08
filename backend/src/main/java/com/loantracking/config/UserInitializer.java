4ad64aaed162c89ff2de759968a4130fcf1874d8
Conrad-Tinio <lstinio@up.edu.ph>
2026-01-08 09:18:18 +0800

    reconfigured due to database connection issues

--- backend/src/main/java/com/loantracking/config/UserInitializer.java
+++ backend/src/main/java/com/loantracking/config/UserInitializer.java
@@ -2,8 +2,9 @@ package com.loantracking.config;
 
 import com.loantracking.model.Person;
 import com.loantracking.repository.PersonRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,13 +13,18 @@ public class UserInitializer {
     @Autowired
     private PersonRepository personRepository;
     
-    @PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
     public void initializeParentUser() {
-        personRepository.findByFullName(UserConfig.PARENT_USER_NAME)
-                .orElseGet(() -> {
-                    Person parentUser = new Person();
-                    parentUser.setFullName(UserConfig.PARENT_USER_NAME);
-                    return personRepository.save(parentUser);
-                });
+        try {
+            personRepository.findByFullName(UserConfig.PARENT_USER_NAME)
+                    .orElseGet(() -> {
+                        Person parentUser = new Person();
+                        parentUser.setFullName(UserConfig.PARENT_USER_NAME);
+                        return personRepository.save(parentUser);
+                    });
+        } catch (Exception e) {
+            // Log error but don't fail application startup
+            System.err.println("Warning: Could not initialize parent user: " + e.getMessage());
+        }
     }
 }
